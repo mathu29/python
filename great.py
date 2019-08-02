d98b4f6e010256546e8b4200c3d83e29dec6c259
mathu29 <53596744+mathu29@users.noreply.github.com>
2019-08-02 14:40:45 +0530

    Create great.py

---
+++ great.py
@@ -0,0 +1,9 @@
+a=int(input("enter the first value"))
+b=int(input("enter the second value"))
+c=int(input("enter the third value"))
+if a>b:
+    print("a is greater")
+elif b>c:
+    print("b is greater")
+else:
+    printf("c is greater")
